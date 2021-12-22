9ad8daacd2f9d48a03473bb9053c944fcfb58c41
--///--//-- <14398606+s9rA16Bf4@users.noreply.github.com>
2021-12-22 19:04:39 +0100

    Quick fix

--- examples/keyboard.ml
+++ examples/keyboard.ml
@@ -1,8 +1,13 @@
-main:{
 
+main:{
+	keyboard.lock(); @ Locks the keyboard
+	
 	window.x("600");
 	window.y("800");
 	window.title("You just got hacked bruh!");
-	window.url("https://mrskeltal.com/");
+	window.url("https://mrskeltal.com/")
 	window.run(); @ Utilizes a builtin print function
+	
+	keyboard.unlock(); @ Unlocks the keyboard
+	system.exit(); @ Exits the program
 }
---
+++ examples/window.ml
@@ -0,0 +1,7 @@
+main:{
+	window.x("600");
+	window.y("800");
+	window.title("You just got hacked bruh!");
+	window.url("https://mrskeltal.com/");
+	window.run(); @ Utilizes a builtin print function
+}
