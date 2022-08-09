f285a2ae98293fecb8584d01e697b41c1617168a
--///--//-- <14398606+s9rA16Bf4@users.noreply.github.com>
2022-08-09 08:56:21 +0200

    Updated the arguments as the argparser being used has also been updated

--- go.mod
+++ go.mod
@@ -4,18 +4,18 @@ go 1.17
 
 require (
 	github.com/thanhpk/randstr v1.0.4 // direct
-	github.com/webview/webview v0.0.0-20220412204103-5d72732c9f5c // direct
+	github.com/webview/webview v0.0.0-20220729131735-25e7f41b8bbf // direct
 )
 
 require (
-	github.com/s9rA16Bf4/ArgumentParser v0.0.0-20220415103352-52aa982165ae // direct
+	github.com/s9rA16Bf4/ArgumentParser v0.0.0-20220809061200-6973cc913c86 // direct
 	github.com/s9rA16Bf4/notify_handler v0.0.0-20220414214026-7cbc65f663ea // direct
 )
 
 require (
 	github.com/TwiN/go-pastebin v1.0.2 // indirect
 	github.com/cloudfoundry/jibber_jabber v0.0.0-20151120183258-bcc4c8345a21 // indirect
-	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
-	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
+	golang.org/x/sys v0.0.0-20220808155132-1c4a2a72c664 // indirect
 	gopkg.in/go-rillas/subprocess.v1 v1.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"runtime"
 
 	arg "github.com/s9rA16Bf4/ArgumentParser/go/arguments"
@@ -14,61 +13,65 @@ import (
 )
 
 func main() {
-	arg.Argument_add("--help", "-h", false, "Shows all available arguments and their purpose", []string{"NULL"})
-	arg.Argument_add("--platform", "-p", true, "For which platform should the malware be compiled for", []string{"darwin", "linux", "windows"})
-	arg.Argument_add("--architecture", "-a", true, "For which architecture should the malware be compiled for", []string{"amd64", "i386"})
-	arg.Argument_add("--file", "-f", true, "File to compile [REQUIRED]", []string{"NULL"})
-	arg.Argument_add("--verbose", "-vv", true, "How verbose should the program be", []string{"0", "1", "2", "3"})
-	arg.Argument_add("--debug", "-d", true, "Debug options, will not delete .go file after compile", []string{"false", "true"})
-	arg.Argument_add("--version", "-v", false, "Prints the compiler version", []string{"NULL"})
-	arg.Argument_add("--output", "-o", true, "Name of the binary malware", []string{"NULL"})
-	arg.Argument_add("--extension", "-e", true, "Extension of the binary malware", []string{"NULL"})
-	arg.Argument_add("--json", "-j", false, "Prints the finalized json structure after compiling a file", []string{"NULL"})
+	arg.Argument_add_with_options("--platform", "-p", true, "For which platform should the malware be compiled for", []string{"darwin", "linux", "windows"})
+	arg.Argument_add_with_options("--architecture", "-a", true, "For which architecture should the malware be compiled for", []string{"amd64", "i386"})
+	arg.Argument_add("--file", "-f", true, "File to compile [REQUIRED]")
+	arg.Argument_add_with_options("--verbose", "-vv", true, "How verbose should the program be", []string{"0", "1", "2", "3"})
+	arg.Argument_add_with_options("--debug", "-d", true, "Debug options, will not delete .go file after compile", []string{"false", "true"})
+	arg.Argument_add("--version", "-v", false, "Prints the compiler version")
+	arg.Argument_add("--output", "-o", true, "Name of the binary malware")
+	arg.Argument_add("--extension", "-e", true, "Extension of the binary malware")
+	arg.Argument_add("--json", "-j", false, "Prints the finalized json structure after compiling a file")
 
-	arg.Argument_parse() // Lets check what the user entered
+	parsed_flags := arg.Argument_parse() // Lets check what the user entered
 	json_object := json.Create_object()
 	json_object.Host_OS = runtime.GOOS
 	json_object.Append_to_call("main")
 
-	if len(os.Args[0:]) > 1 { // The user entered something
-		if arg.Argument_check("-h") {
-			arg.Argument_help()
-		} else if arg.Argument_check("-v") {
+	if len(parsed_flags) > 0 { // The user entered something
+		if _, entered := parsed_flags["-v"]; entered {
 			version.Print_version()
+
 		} else {
-			if arg.Argument_check("-p") { // The user specificed a target platform
-				json_object.Target_OS = arg.Argument_get("-tp")
+
+			if value, entered := parsed_flags["-p"]; entered { // The user specificed a target platform
+				json_object.Target_OS = value
 			} else {
 				json_object.Target_OS = runtime.GOOS
 			}
-			if arg.Argument_check("-a") {
-				json_object.Target_ARCH = arg.Argument_get("-ta")
+
+			if value, entered := parsed_flags["-a"]; entered {
+				json_object.Target_ARCH = value
 			} else {
 				json_object.Target_ARCH = runtime.GOARCH
 			}
-			if !arg.Argument_check("-f") {
-				notify.Error("The '--file'/'-f' flag was not passed.", "main.main()")
+
+			if value, entered := parsed_flags["-f"]; entered {
+				json_object.File = value // Get the file
+
 			} else {
-				json_object.File = arg.Argument_get("-f") // Get and remember the file
+				notify.Error("The '--file'/'-f' flag was not passed.", "main.main()")
 			}
-			if arg.Argument_check("-vv") {
-				json_object.Verbose_LVL = arg.Argument_get("-vv")
+
+			if value, entered := parsed_flags["-vv"]; entered {
+				json_object.Verbose_LVL = value
 				notify.Log(fmt.Sprintf("Setting verbose level to %s", json_object.Verbose_LVL), json_object.Verbose_LVL, "1")
 			} else {
 				json_object.Verbose_LVL = "0"
 			}
 
-			if arg.Argument_check("-d") && arg.Argument_get("-d") == "true" {
+			if value, entered := parsed_flags["-d"]; entered && (value == "True" || value == "true") {
 				json_object.DebugMode = true
 			} else {
 				json_object.DebugMode = false
 			}
 
-			if arg.Argument_check("-o") {
-				json_object.Set_binary_name(arg.Argument_get("-o"))
+			if value, entered := parsed_flags["-o"]; entered {
+				json_object.Set_binary_name(value)
 			}
-			if arg.Argument_check("-e") {
-				json_object.Set_Extension(arg.Argument_get("-e"))
+
+			if value, entered := parsed_flags["-e"]; entered {
+				json_object.Set_Extension(value)
 			}
 
 			notify.Log(fmt.Sprintf("File to compile is '%s'", json_object.File), json_object.Verbose_LVL, "1")
@@ -83,7 +86,7 @@ func main() {
 			data_structure = json.Receive(io.Write_file(json.Send(data_structure)))   // The interpreter has filled the internal array with the correct go code, so this will dump it to a file
 			data_structure = json.Receive(io.Compile_file(json.Send(data_structure))) // This compiles the previously written code into a functioan program
 
-			if arg.Argument_check("-j") {
+			if _, entered := parsed_flags["-j"]; entered {
 				fmt.Println(string(json.Convert_to_json(data_structure)))
 			}
 		}
