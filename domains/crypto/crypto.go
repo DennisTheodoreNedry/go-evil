d41115da5401fcb8fa9d0f1f161afee711e3c381
--///--//-- <14398606+s9rA16Bf4@users.noreply.github.com>
2022-10-03 15:57:28 +0200

    base crypto module wip

--- domains/crypto/crypto.go
+++ domains/crypto/crypto.go
@@ -1,10 +1,39 @@
 package crypto
 
-//
-//
-//
-//
-//
-func encrypt(values string, s_json string) {
+import "github.com/TeamPhoneix/go-evil/utility/structure"
 
+//
+//
+// Encrypts the provided object
+// The input must follow this format '${"<encryption system>", "<key>", "<file/ext/dir>", "<path/to/object>"}$'
+//
+//
+func encrypt(value string, s_json string) string {
+	data_object := structure.Receive(s_json)
+
+	return structure.Send(data_object)
+}
+
+func set_method(value string, s_json string) string {
+	data_object := structure.Receive(s_json)
+
+	return structure.Send(data_object)
+}
+
+func set_key(value string, s_json string) string {
+	data_object := structure.Receive(s_json)
+
+	return structure.Send(data_object)
+}
+
+func set_target(value string, s_json string) string {
+	data_object := structure.Receive(s_json)
+
+	return structure.Send(data_object)
+}
+
+func decrypt(value string, s_json string) string {
+	data_object := structure.Receive(s_json)
+
+	return structure.Send(data_object)
 }
--- domains/crypto/parser.go
+++ domains/crypto/parser.go
@@ -16,6 +16,19 @@ func Parser(function string, value string, s_json string) ([]string, string) {
 
 	switch function {
 	case "encrypt":
+		s_json = encrypt(value, s_json)
+
+	case "set_method":
+		s_json = set_method(value, s_json)
+
+	case "set_key":
+		s_json = set_key(value, s_json)
+
+	case "set_target":
+		s_json = set_target(value, s_json)
+
+	case "decrypt":
+		s_json = decrypt(value, s_json)
 
 	default:
 		notify.Error(fmt.Sprintf("Unknown function '%s'", function), "system.Parser()")
