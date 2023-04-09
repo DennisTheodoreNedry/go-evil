6038a2e5aec290e782ec4df9ce9ac630418413fc
--///--//-- <14398606+s9rA16Bf4@users.noreply.github.com>
2023-04-09 10:05:13 +0200

    Refractored the self domain

--- domains/self/parser.go
+++ domains/self/parser.go
@@ -3,35 +3,36 @@ package self
 import (
 	"fmt"
 
+	evil_call "github.com/TeamPhoneix/go-evil/domains/self/call"
+	"github.com/TeamPhoneix/go-evil/domains/self/include"
+	"github.com/TeamPhoneix/go-evil/domains/self/random"
+	"github.com/TeamPhoneix/go-evil/domains/self/set"
+
 	"github.com/s9rA16Bf4/notify_handler/go/notify"
 )
 
-//
-//
 // The main parser for the self domain
-//
-//
 func Parser(function string, value string, s_json string) ([]string, string) {
 	call := []string{}
 
 	switch function {
 	case "call":
-		call, s_json = Call_function(value, s_json)
+		call, s_json = evil_call.Function(value, s_json)
 
 	case "include":
-		s_json = Include(value, s_json)
+		s_json = include.Include(value, s_json)
 
 	case "set_run":
-		call, s_json = Set(false, value, s_json)
+		call, s_json = set.Set(false, value, s_json)
 
 	case "set_comp":
-		call, s_json = Set(true, value, s_json)
+		call, s_json = set.Set(true, value, s_json)
 
 	case "add_random_var":
-		s_json = Add_random_variable(value, s_json)
+		s_json = random.Add_variable(value, s_json)
 
 	case "add_random_func":
-		call, s_json = Add_random_function(value, s_json)
+		call, s_json = random.Add_function(value, s_json)
 
 	default:
 		notify.Error(fmt.Sprintf("Unknown function '%s'", function), "system.Parser()")
--- domains/self/set/set.go
+++ domains/self/set/set.go
@@ -1,4 +1,4 @@
-package self
+package set
 
 import (
 	"fmt"
