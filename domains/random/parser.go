a669ef793fcc5099172bae9a358606be589dda7b
--///--//-- <14398606+s9rA16Bf4@users.noreply.github.com>
2023-04-09 10:01:48 +0200

    Refractored the random domain

--- domains/random/parser.go
+++ domains/random/parser.go
@@ -3,23 +3,22 @@ package random
 import (
 	"fmt"
 
+	evil_int "github.com/TeamPhoneix/go-evil/domains/random/int"
+	evil_string "github.com/TeamPhoneix/go-evil/domains/random/string"
+
 	"github.com/s9rA16Bf4/notify_handler/go/notify"
 )
 
-//
-//
 // The main parser for the random domain
-//
-//
 func Parser(function string, value string, s_json string) ([]string, string) {
 	call := []string{}
 
 	switch function {
 	case "int":
-		s_json = generate_int(value, s_json)
+		s_json = evil_int.Generate(value, s_json)
 
 	case "string":
-		s_json = generate_string(value, s_json)
+		s_json = evil_string.Generate(value, s_json)
 
 	default:
 		notify.Error(fmt.Sprintf("Unknown function '%s'", function), "system.Parser()")
