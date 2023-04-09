d69e9f8887e151545a69e37991a1d780a4c2bf69
--///--//-- <14398606+s9rA16Bf4@users.noreply.github.com>
2023-04-09 10:06:51 +0200

    Refractored the time domain

--- domains/time/parser.go
+++ domains/time/parser.go
@@ -3,23 +3,22 @@ package time
 import (
 	"fmt"
 
+	"github.com/TeamPhoneix/go-evil/domains/time/sleep"
+	"github.com/TeamPhoneix/go-evil/domains/time/until"
+
 	"github.com/s9rA16Bf4/notify_handler/go/notify"
 )
 
-//
-//
 // The main parser for the time domain
-//
-//
 func Parser(function string, value string, s_json string) ([]string, string) {
 	call := []string{}
 
 	switch function {
 	case "until":
-		call, s_json = Until(s_json, value)
+		call, s_json = until.Until(s_json, value)
 
 	case "sleep":
-		call, s_json = Sleep(s_json, value)
+		call, s_json = sleep.Sleep(s_json, value)
 
 	default:
 		notify.Error(fmt.Sprintf("Unknown function '%s'", function), "system.Parser()")
