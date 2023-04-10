d6b14a39e2257a60dea96b19448ede3ee2da3cc6
--///--//-- <14398606+s9rA16Bf4@users.noreply.github.com>
2023-04-10 15:42:48 +0200

    updated the base64 domain

--- domains/base64/encode/encode.go
+++ domains/base64/encode/encode.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/TeamPhoneix/go-evil/utility/structure"
+	"github.com/TeamPhoneix/go-evil/utility/structure/functions"
 )
 
 // Encodes the provided string and places the result in a runtime variable
@@ -11,11 +12,12 @@ func Encode(value string, s_json string) ([]string, string) {
 	data_object := structure.Receive(s_json)
 	function_call := "base64_encode"
 
-	data_object.Add_go_function([]string{
-		fmt.Sprintf("func %s(repr_1 []int){", function_call),
-		"value1 := spine.variable.get(spine.alpha.construct_string(repr_1))",
-		"spine.variable.set(coldfire.B64E(value1))",
-		"}"})
+	data_object.Add_go_function(functions.Go_func_t{Name: function_call, Func_type: "", Part_of_struct: "", Return_type: "",
+		Parameters: []string{"repr_1 []int"},
+		Gut: []string{
+			"value1 := spine.variable.get(spine.alpha.construct_string(repr_1))",
+			"spine.variable.set(coldfire.B64E(value1))",
+		}})
 
 	data_object.Add_go_import("github.com/redcode-labs/Coldfire")
 
