daa1f56a8409343377320ada5945875569fc6af7
--///--//-- <14398606+s9rA16Bf4@users.noreply.github.com>
2023-04-10 14:15:00 +0200

    updated the window domain

--- domains/window/navigate/navigate.go
+++ domains/window/navigate/navigate.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/TeamPhoneix/go-evil/utility/structure"
+	"github.com/TeamPhoneix/go-evil/utility/structure/functions"
 )
 
 // Makes the window enter a website of your choice
@@ -11,18 +12,19 @@ func Navigate(website string, s_json string) ([]string, string) {
 	data_object := structure.Receive(s_json)
 	function_call := "Navigate"
 
-	data_object.Add_go_function([]string{
-		fmt.Sprintf("func %s(repr_1 []int){", function_call),
-		"website := spine.variable.get(spine.alpha.construct_string(repr_1))",
-		fmt.Sprintf("win, err := lorca.New(website, \"\",%d, %d)", data_object.Width, data_object.Height),
-		"if err != nil{",
-		"spine.log(err.Error())",
-		"return",
-		"}",
-		"defer win.Close()",
-		"<-win.Done()",
-		"}",
-	})
+	data_object.Add_go_function(functions.Go_func_t{Name: function_call, Func_type: "", Part_of_struct: "", Return_type: "",
+		Parameters: []string{"repr_1 []int"},
+
+		Gut: []string{
+			"website := spine.variable.get(spine.alpha.construct_string(repr_1))",
+			fmt.Sprintf("win, err := lorca.New(website, \"\",%d, %d)", data_object.Width, data_object.Height),
+			"if err != nil{",
+			"spine.log(err.Error())",
+			"return",
+			"}",
+			"defer win.Close()",
+			"<-win.Done()",
+		}})
 
 	data_object.Add_go_import("github.com/zserge/lorca")
 	data_object.Add_go_import("github.com/s9rA16Bf4/notify_handler/go/notify")
