adabbc7af137dc2314ead33f0bd2a50f83048173
s9rA16Bf4 <14398606+s9rA16Bf4@users.noreply.github.com>
2023-07-03 07:34:29 +0200

    Moved utility/tools and utility/test into a new repo

--- utility/tools/generate_random_bool.go
+++
@@ -1,7 +0,0 @@
-package tools
-
-// Generates a random bool
-func Generate_random_bool() bool {
-	value := Generate_random_int_between(0, 2)
-	return value == 1
-}
--- utility/tools/int_to_string.go
+++
@@ -1,8 +0,0 @@
-package tools
-
-import "fmt"
-
-// Converts the provided int to a string
-func Int_to_string(value int) string {
-	return fmt.Sprint(value)
-}
